dba9ee5ab3afc1c07b6df5b5c381becc30ff8ad3
adcerro <adcerro@uninorte.edu.co>
2024-05-17 16:28:40 -0500

    fibonacci sequence

---
+++ Fibo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{
+    printf("Entra a fibonacci %d de padre %s\n", getpid(), argv[argc - 1]);
+    if (argc != 3)
+    {
+        fprintf(stderr, "Uso: fibo lim-fibo\n");
+        return 1;
+    }
+    int number = atoi(argv[1]);
+    if (number == 1)
+    {
+        printf("Fibo 1 = 0\n");
+        return 0;
+    }
+    if (number == 2)
+    {
+        printf("Fibo 1 = 0\n");
+        printf("Fibo 2 = 1\n");
+        return 0;
+    }
+    int sequence[number];
+    sequence[0] = 0;
+    sequence[1] = 1;
+    printf("Fibo 1 = 0\n");
+    printf("Fibo 2 = 1\n");
+    for (int i = 2; i < number; i++)
+    {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+        printf("Fibo %d = %d\n", i + 1, sequence[i]);
+    }
+}
